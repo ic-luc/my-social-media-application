7794281b148042d5accdc7cf7d031997dcd02ddd
ic-luc <flucic097@gmail.com>
2025-04-16 17:35:39 +0200

    Full Stack Web Development Course by PedroTech (YouTube), episode 08

--- server/server.js
+++ server/server.js
@@ -14,6 +14,8 @@ const postRouter = require('./routes/Posts');
 app.use("/posts", postRouter); // Middleware for router
 const commentsRouter = require('./routes/Comments');
 app.use("/comments", commentsRouter); // Middleware for router
+const usersRouter = require('./routes/Users');
+app.use("/auth", usersRouter); // Middleware for router
 
 db.sequelize.sync().then(() => {
   // Starting / running the API
